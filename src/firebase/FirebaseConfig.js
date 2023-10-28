df2de16835e73e6838b2b6332c07d1d0759b9eaf
Shafiulla Attar <96431513+shafi78@users.noreply.github.com>
2023-10-28 23:39:23 +0530

    Update FirebaseConfig.js

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAL-MTQGolS39KAjMlenXn3v1IY0nl8Z60",
-    authDomain: "student-c1cca.firebaseapp.com",
-    projectId: "student-c1cca",
-    storageBucket: "student-c1cca.appspot.com",
-    messagingSenderId: "795493420922",
-    appId: "1:795493420922:web:512dc73cd278b5e71a50c3",
-    measurementId: "G-TN2M59EKQE"
+   s
   };
 
 
